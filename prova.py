29993c8c7e8d9ec43602feb74892286174dc11ae
Chiara Alberti <achiara1995@gmail.com>
2019-12-18 20:30:45 +0000

    Removed useless file

--- prova.py
+++
@@ -1,4 +0,0 @@
-from netdict import Net_type
-
-
-print(Net_type(0,0,0)['cpu_net'])
--- provq.py
+++
@@ -1,6 +0,0 @@
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-
-img = cv2.imread('/home/stormlab/seg/lstm_dataset/train/disp_lobe1_01013.jpeg', -1).astype(np.float32)
-plt.imshow(img/255)
